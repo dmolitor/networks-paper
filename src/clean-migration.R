11aacdfedc14e839477e627c93c673e14e5b9a98
dmolitor <molitdj97@gmail.com>
2023-03-25 14:32:21 -0400

    Updated definition of "urban" in migration data :bus:

--- src/clean-migration.R
+++ src/clean-migration.R
@@ -24,18 +24,21 @@ counties <- dplyr::left_join(counties, states, by = "STATEFP") |>
 
 # Pull in urban areas
 urban_des <- janitor::clean_names(
-  readxl::read_excel(here::here("scratch/rural_urban.xlsx"))
+  readxl::read_excel(here::here("scratch/rural_urban.xls"), skip = 2)
 )
-counties <- counties |>
-  dplyr::left_join(
-    dplyr::distinct(
-      dplyr::mutate(dplyr::select(urban_des, cty_fips), urban = 0),
-      cty_fips,
-      .keep_all = TRUE
-    ), 
-    by = c("FIPS" = "cty_fips")
+urban_des <- urban_des |>
+  dplyr::filter(
+    metropolitan_micropolitan_statistical_area == "Metropolitan Statistical Area"
   ) |>
-  dplyr::mutate(urban = ifelse(is.na(urban), 1, 0))
+  dplyr::mutate(
+    fips = paste0(fips_state_code, fips_county_code),
+    urban = 1
+  ) |>
+  dplyr::select(fips, urban)
+
+counties <- counties |>
+  dplyr::left_join(urban_des, by = c("FIPS" = "fips")) |>
+  dplyr::mutate(urban = ifelse(is.na(urban), 0, 1))
 
 # Combine all sheets from the Excel file
 migration <- dplyr::bind_rows(
@@ -146,12 +149,34 @@ migration <- migration |>
 # Add a binary indicator: 1 = County intersects urban area; 0 = No urban areas
 migration <- migration |>
   dplyr::left_join(
-    dplyr::select(counties, FIPS, urban),
+    dplyr::select(counties, FIPS, county, state, urban),
     by = c("o_fips" = "FIPS")
+  ) |>
+  dplyr::rename(
+    "o_county" = "county",
+    "o_state" = "state",
+    "o_urban" = "urban"
+  ) |>
+  dplyr::left_join(
+    dplyr::select(sf::st_drop_geometry(counties), FIPS, county, state, urban),
+    by = c("d_fips" = "FIPS")
+  ) |>
+  dplyr::rename(
+    "d_county" = "county",
+    "d_state" = "state",
+    "d_urban" = "urban"
+  ) |>
+  dplyr::mutate(
+    migration_type = dplyr::case_when(
+      o_urban == 0 & d_urban == 0 ~ "rural-rural",
+      o_urban == 0 & d_urban == 1 ~ "rural-urban",
+      o_urban == 1 & d_urban == 0 ~ "urban-rural",
+      o_urban == 1 & d_urban == 1 ~ "urban-urban"
+    )
   )
 
 # Output data
-migration_out <- sf::st_drop_geometry(migration) |> dplyr::select(-geometry)
+migration_out <- dplyr::select(migration, -geometry)
 readr::write_csv(migration_out, here::here("data/migration.csv"))
 
 # Plotting ----------------------------------------------------------------
@@ -162,7 +187,7 @@ tigris::shift_geometry(counties) |>
   ggplot(aes(fill = Urban)) + 
   geom_sf() + 
   theme_light() +
-  labs(title = "Urban/Rural Counties (as Classified by the HRSA)") +
+  labs(title = "Urban/Rural Counties (as Classified by the US Census)") +
   theme(
     axis.text = element_blank(),
     panel.grid = element_blank(),
@@ -170,4 +195,222 @@ tigris::shift_geometry(counties) |>
     plot.title = element_text(hjust = 0.5)
   )
 
+# Plot counties by net migration to that county
+migration_agg <- migration |>
+  dplyr::mutate(n_net = as.numeric(n_net)) |>
+  dplyr::group_by(o_fips, o_county, o_state) |>
+  dplyr::summarise(
+    n_net = sum(n_net),
+    urban = dplyr::first(o_urban),
+    geometry = dplyr::first(geometry),
+    .groups = "drop"
+  ) |>
+  sf::st_as_sf()
 
+# Get bin values for inflows/outflows
+outflow_bins <- dplyr::filter(migration_agg, n_net >= 0) |>
+  dplyr::pull(n_net) |>
+  quantile(seq(0, 1, length.out = 4))
+
+inflow_bins <- dplyr::filter(migration_agg, n_net <= 0) |>
+  dplyr::pull(n_net) |>
+  quantile(seq(0, 1, length.out = 4))
+
+flow_bins <- unique(unname(c(inflow_bins, outflow_bins)))
+flow_labels <- c(
+  "High Inflow", "Medium Inflow", "Low Inflow",
+  "Low Outflow", "Medium Outflow", "High Outflow"
+)
+
+# Plot it
+migration_agg_plot_dat <- tigris::shift_geometry(migration_agg) |>
+  dplyr::mutate(
+    `Net Migration` = cut(
+      n_net,
+      breaks = flow_bins,
+      labels = flow_labels,
+      include.lowest = TRUE,
+      ordered_result = TRUE
+    )
+  )
+
+### Counties (urban vs. rural) migration flows
+
+# All counties
+ggplot(tigris::shift_geometry(counties)) +
+  geom_sf(fill = "gray") +
+  geom_sf(
+    data = migration_agg_plot_dat,
+    mapping = aes(fill = `Net Migration`),
+    inherit.aes = FALSE
+  ) +
+  geom_sf(
+    data = tigris::shift_geometry(tigris::states(TRUE, "20m", 2015)),
+    alpha = 0,
+    color = "white",
+    size = 0.5,
+    inherit.aes = FALSE
+  ) +
+  scale_fill_brewer(type = "div", palette = "RdYlGn", direction = -1) +
+  labs(title = "Net Migration by County (2011-2015)") +
+  theme(
+    axis.text = element_blank(),
+    axis.ticks = element_blank(),
+    legend.background = element_rect(fill = "#F5F5F2", color = NA),
+    legend.title = element_blank(),
+    panel.background = element_rect(fill = "#F5F5F2", color = NA),
+    panel.grid = element_blank(),
+    panel.border = element_blank(),
+    plot.background = element_rect(fill = "#F5F5F2", color = NA),
+    plot.title = element_text(face = "bold", hjust = 0.5, size = 18),
+    text = element_text(family = "PT Sans")
+  )
+
+# Urban counties
+ggplot(tigris::shift_geometry(counties)) +
+  geom_sf(fill = "gray") +
+  geom_sf(
+    data = dplyr::filter(migration_agg_plot_dat, urban == 1),
+    mapping = aes(fill = `Net Migration`),
+    inherit.aes = FALSE
+  ) +
+  geom_sf(
+    data = tigris::shift_geometry(tigris::states(TRUE, "20m", 2015)),
+    alpha = 0,
+    color = "white",
+    size = 0.5,
+    inherit.aes = FALSE
+  ) +
+  scale_fill_brewer(type = "div", palette = "RdYlGn", direction = -1) +
+  labs(title = "Net Migration in Urban Counties (2011-2015)") +
+  theme(
+    axis.text = element_blank(),
+    axis.ticks = element_blank(),
+    legend.background = element_rect(fill = "#F5F5F2", color = NA),
+    legend.title = element_blank(),
+    panel.background = element_rect(fill = "#F5F5F2", color = NA),
+    panel.grid = element_blank(),
+    panel.border = element_blank(),
+    plot.background = element_rect(fill = "#F5F5F2", color = NA),
+    plot.title = element_text(face = "bold", hjust = 0.5, size = 18),
+    text = element_text(family = "PT Sans")
+  )
+
+# Non-urban counties
+ggplot(tigris::shift_geometry(counties)) +
+  geom_sf(fill = "gray") +
+  geom_sf(
+    data = dplyr::filter(migration_agg_plot_dat, urban == 0),
+    mapping = aes(fill = `Net Migration`),
+    inherit.aes = FALSE
+  ) +
+  geom_sf(
+    data = tigris::shift_geometry(tigris::states(TRUE, "20m", 2015)),
+    alpha = 0,
+    color = "white",
+    size = 0.5,
+    inherit.aes = FALSE
+  ) +
+  scale_fill_brewer(type = "div", palette = "RdYlGn", direction = -1) +
+  labs(title = "Net Migration in Non-Urban Counties (2011-2015)") +
+  theme(
+    axis.text = element_blank(),
+    axis.ticks = element_blank(),
+    legend.background = element_rect(fill = "#F5F5F2", color = NA),
+    legend.title = element_blank(),
+    panel.background = element_rect(fill = "#F5F5F2", color = NA),
+    panel.grid = element_blank(),
+    panel.border = element_blank(),
+    plot.background = element_rect(fill = "#F5F5F2", color = NA),
+    plot.title = element_text(face = "bold", hjust = 0.5, size = 18),
+    text = element_text(family = "PT Sans")
+  )
+
+### 
+
+# Chord diagram
+
+top_rural_to_urban <- migration |>
+  dplyr::filter(migration_type == "rural-urban") |>
+  dplyr::summarise(
+    n = sum(as.numeric(n_net)),
+    .by = c(o_fips, o_county, o_state)
+  ) |>
+  dplyr::arrange(dplyr::desc(n)) |>
+  dplyr::slice_head(n = 5) |>
+  dplyr::mutate(o_name = paste0(o_county, ", ", o_state))
+
+top_rural_to_urban <- migration |>
+  dplyr::filter(migration_type == "rural-urban") |>
+  dplyr::mutate(
+    o_name = paste0(o_county, ", ", o_state),
+    d_name = paste0(d_county, ", ", d_state)
+  ) |>
+  dplyr::filter(o_name %in% top_rural_to_urban$o_name) |>
+  dplyr::mutate(
+    o_state = state.abb[match(o_state, state.name)],
+    d_state = state.abb[match(d_state, state.name)],
+    o_name = paste0(o_county, ", ", o_state),
+    d_name = paste0(d_county, ", ", d_state)
+  ) |>
+  dplyr::arrange(dplyr::desc(n)) |>
+  dplyr::group_by(o_county) |>
+  dplyr::slice_head(n = 5) |>
+  dplyr::ungroup() |>
+  dplyr::arrange(o_fips) |>
+  dplyr::mutate(
+    to = paste0(d_county, ", ", substr(d_state, 1, 3)), 
+    from = paste0(o_county, ", ", substr(o_state, 1, 3)),
+    value = n,
+    type = "Rural to Urban"
+  ) |>
+  dplyr::select(from, to, value, type)
+
+# top_urban_to_rural <- migration |>
+#   dplyr::filter(migration_type == "urban-rural") |>
+#   dplyr::arrange(dplyr::desc(pr_d_o)) |>
+#   dplyr::slice_head(n = 30) |>
+#   dplyr::mutate(
+#     to = paste0(d_county, ", ", d_state), 
+#     from = paste0(o_county, ", ", o_state),
+#     type = "Urban to Rural"
+#   ) |>
+#   dplyr::select(from, to, n, pr_d_o, type)
+# 
+# top_switches <- dplyr::bind_rows(top_rural_to_urban, top_urban_to_rural) |>
+#   dplyr::rename("value" = "pr_d_o")
+
+# Plot it!!!
+circlize::chordDiagram(
+  x = top_rural_to_urban[, c("from", "to", "value")],
+  row.col = 1:5,
+  directional = 1,
+  direction.type = c("diffHeight", "arrows"),
+  link.arr.type = "big.arrow",
+  annotationTrack = "grid",
+  preAllocateTracks = 1
+)
+circos.trackPlotRegion(
+  track.index = 1,
+  panel.fun = function(x, y) {
+    xlim = get.cell.meta.data("xlim")
+    ylim = get.cell.meta.data("ylim")
+    sector.name = get.cell.meta.data("sector.index")
+    circos.text(
+      mean(xlim),
+      ylim[1] + .1,
+      sector.name,
+      facing = "clockwise",
+      niceFacing = TRUE,
+      adj = c(0, 0.5)
+    )
+    circos.axis(
+      h = "top",
+      labels.cex = 0.5,
+      major.tick.length = 0.2,
+      sector.index = sector.name,
+      track.index = 2
+    )
+  },
+  bg.border = NA
+)
--- src/download-migration.py
+++ src/download-migration.py
@@ -1,7 +1,7 @@
 import requests
 
 url = "https://www2.census.gov/programs-surveys/demo/tables/geographic-mobility/2015/county-to-county-migration-2011-2015/county-to-county-migration-flows/county-to-county-2011-2015-ins-outs-nets-gross.xlsx"
-rural_url = "https://www.hrsa.gov/sites/default/files/hrsa/rural-health/non-metro-counties-cts.xlsx"
+rural_url = "https://www2.census.gov/programs-surveys/metro-micro/geographies/reference-files/2015/delineation-files/list1.xls"
 resp = requests.get(url)
 
 with open("scratch/total_migration.xlsx", "wb") as f:
@@ -9,5 +9,5 @@ with open("scratch/total_migration.xlsx", "wb") as f:
 
 resp = requests.get(rural_url)
 
-with open("scratch/rural_urban.xlsx", "wb") as f:
+with open("scratch/rural_urban.xls", "wb") as f:
     f.write(resp.content)

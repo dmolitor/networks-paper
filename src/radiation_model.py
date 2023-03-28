cc7436004c014417b5a7b21698ce1828a1f568e8
dmolitor <molitdj97@gmail.com>
2023-03-27 22:18:14 -0400

    Code for gravity and radiation models :earth_asia:

--- src/radiation_model.py
+++ src/radiation_model.py
@@ -5,7 +5,10 @@ import pygris
 from pygris.utils import shift_geometry
 from random import sample
 import skmob
+from skmob.models.gravity import Gravity
 from skmob.models.radiation import Radiation
+import tempfile
+import webbrowser
 
 def states():
     return [
@@ -78,6 +81,7 @@ cz = cz.merge(
 cz = (
     cz
     .drop("o_cz", axis=1)
+    .drop_duplicates(subset="commuting_zone_id")
     .rename(columns={"n_tot_o":"population", "commuting_zone_id":"tile_ID"})
 )
 
@@ -105,16 +109,122 @@ cz = (
     .rename(columns={"flow": "tot_outflow"})
 )
 
-# Build radiation model
-subsample = sample(list(range(1, len(cz))), round(.01*len(cz)))
-subsample.sort()
-cz_subsample = cz.iloc[subsample]
+# Build gravity model
 
+gravity = Gravity()
+gravity.fit(flows, relevance_column="population")
+gravity_flows = gravity.generate(
+    spatial_tessellation=cz,
+    tile_id_column="tile_ID",
+    tot_outflows_column="tot_outflow",
+    relevance_column= "population",
+    out_format="flows"
+)
+
+# Build radiation model
 radiation = Radiation()
 rad_flows = radiation.generate(
-    spatial_tessellation=cz_subsample,
+    spatial_tessellation=cz,
     tile_id_column="tile_ID",
     tot_outflows_column="tot_outflow",
     relevance_column="population",
     out_format="flows"
 )
+
+# Compare predicted and actual flows
+def compare_flows(name, min_flow=50, opacity=.5):
+    tile_id = cz[cz["commuting_zone_name"] == name]["tile_ID"].tolist()
+    pred_flows = rad_flows[rad_flows["origin"] == str(tile_id[0])]
+    pred_grav_flows = gravity_flows[gravity_flows["origin"] == str(tile_id[0])]
+    real_flows = flows[flows["origin"] == str(tile_id[0])]
+    m0 = pred_grav_flows.plot_flows(
+        flow_color="green",
+        min_flow = min_flow,
+        zoom = 5,
+        opacity=opacity
+    )
+    m1 = pred_flows.plot_flows(
+        flow_color="blue",
+        min_flow = min_flow,
+        zoom = 5,
+        opacity=opacity
+    )
+    m2 = real_flows.plot_flows(
+        flow_color="red",
+        min_flow = min_flow,
+        zoom = 5,
+        opacity=opacity
+    )
+    m0_path = tempfile.mktemp(suffix=".html")
+    m1_path = tempfile.mktemp(suffix=".html")
+    m2_path = tempfile.mktemp(suffix=".html")
+    m0.save(m0_path)
+    m1.save(m1_path)
+    m2.save(m2_path)
+    webbrowser.open("file://" + m0_path)
+    webbrowser.open("file://" + m1_path)
+    webbrowser.open("file://" + m2_path)
+
+# Merge predicted and actual flow counts
+
+## Radiation flows
+flows_rad = flows.merge(
+    rad_flows.rename(columns={"flow":"radius_flow"}),
+    on=["origin", "destination"],
+    how="outer"
+)
+flows_rad["radius_flow"] = flows_rad["radius_flow"].fillna(0).astype(int)
+flows_rad["flow"] = flows_rad["flow"].fillna(0).astype(int)
+
+## Gravity flows
+flows_grav = flows.merge(
+    gravity_flows.rename(columns={"flow":"gravity_flow"}),
+    on=["origin", "destination"],
+    how="outer"
+)
+flows_grav["gravity_flow"] = flows_grav["gravity_flow"].fillna(0).astype(int)
+flows_grav["flow"] = flows_grav["flow"].fillna(0).astype(int)
+
+# Calculate Sorensen-Dice index
+def cpc(data, num_col, denom_col, digits=5):
+    cpc = data[num_col].sum()/data[denom_col].sum()
+    cpc = round(cpc, digits)
+    return cpc
+
+flows_rad["min_g_r_flow"] = flows_rad[["flow", "radius_flow"]].min(axis=1) * 2
+flows_rad["sum_g_r_flow"] = flows_rad[["flow", "radius_flow"]].sum(axis=1)
+
+flows_grav["min_g_r_flow"] = flows_grav[["flow", "gravity_flow"]].min(axis=1) * 2
+flows_grav["sum_g_r_flow"] = flows_grav[["flow", "gravity_flow"]].sum(axis=1)
+
+print(f"CPC Radiation model: {cpc(flows_rad, 'min_g_r_flow', 'sum_g_r_flow')}")
+print(f"CPC Gravity model: {cpc(flows_grav, 'min_g_r_flow', 'sum_g_r_flow')}")
+
+# Aggregate CPC by commuting zone
+cz_gravity_cpc = (
+    flows_grav
+    .groupby("origin", axis=0)
+    .apply(lambda x: cpc(x, "min_g_r_flow", "sum_g_r_flow"))
+    .reset_index()
+    .rename(columns={0:"gravity_cpc"})
+)
+cz_gravity_cpc["origin"] = cz_gravity_cpc["origin"].astype(int)
+
+cz_rad_cpc = (
+    flows_rad
+    .groupby("origin", axis=0)
+    .apply(lambda x: cpc(x, "min_g_r_flow", "sum_g_r_flow"))
+    .reset_index()
+    .rename(columns={0:"rad_cpc"})
+)
+cz_rad_cpc["origin"] = cz_rad_cpc["origin"].astype(int)
+
+cz = cz.merge(cz_gravity_cpc, left_on="tile_ID", right_on="origin", how="left")
+cz = cz.merge(cz_rad_cpc, left_on="tile_ID", right_on="origin", how="left")
+
+# Output CPC as geojso
+cz_out = cz[[
+    "tile_ID", "commuting_zone_name", "state", "geometry", "population",
+    "tot_outflow", "gravity_cpc", "rad_cpc"
+]]
+cz_out.to_file("scratch/cz_cpc_measures.geojson")
